7b33a54728041e2e9d58d15561c4206fb9774529
Michelle <mpuschkarow@web.de>
2025-10-26 18:29:16 +0100

    tk window and canvas created

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+
+canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="images/tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.pack()
+
+window.mainloop()
